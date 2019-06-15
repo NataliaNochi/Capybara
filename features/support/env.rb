e38cc7144d2581fbaaa5c45306181ca8986d2350
NataliaNochi <natalia.nochi@hotmail.com>
2019-06-15 14:32:27 +0100

    ajuste para rodar em headless

--- features/support/env.rb
+++ features/support/env.rb
@@ -16,14 +16,15 @@ when "chrome"
     @driver = :selenium_chrome
 when "headless"
   Capybara.register_driver :selenium_chrome_headless do |app|
-    option.add_argument "--headless"
-    option.add_argument "--disable-gpu"
-    option.add_argument "--no-sandbox"
-    option.add_argument "--disable-site-isolation-trials"
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
   end
-  Capybara::Selenium::Driver.new(app, browser: :chrome, option: chrome_options)
-end
-@driver = :selenium_chrome_headless
+  @driver = :selenium_chrome_headless
 else
     puts "Invalid Browser"
 end
