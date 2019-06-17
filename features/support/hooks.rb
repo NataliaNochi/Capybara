38739ef8ceb98e0a9bda0e7128cc55d79cb6c09a
NataliaNochi <natalia.nochi@hotmail.com>
2019-06-17 11:12:48 +0100

    screenshot

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -10,10 +10,15 @@ Before("@login") do
   @login_page.with(user["email"], user["pass"])
 end
   
+# After do |scenario|
+#   if scenario.failed?
+#     temp_shot = page.save_screenshot("log/temp_shot.png")
+#     screenshot = Base64.encode64(File.open(temp_shot).read)
+#     embed(screenshot, "image/png", "Screenshot")
+#   end
+# end
+
 After do |scenario|
-  if scenario.failed?
-    temp_shot = page.save_screenshot("log/temp_shot.png")
-    screenshot = Base64.encode64(File.open(temp_shot).read)
-    embed(screenshot, "image/png", "Screenshot")
-  end
+ screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+ embed(screenshot, "image/png", "Screenshot")
 end
